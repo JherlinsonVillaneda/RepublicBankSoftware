d3e9f80c2fe928c3de8923530d6562ea86159dda
Jherlinson Stiven Villaneda Carvajal <jherlinson.villaneda@globant.com>
2024-01-15 14:44:12 -0500

    First Commit

---
+++ src/main/java/republicBank/RepublicBankApplication.java
@@ -0,0 +1,13 @@
+package republicBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepublicBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepublicBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/republicBank/RepublicBankApplicationTests.java
@@ -0,0 +1,13 @@
+package republicBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepublicBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
